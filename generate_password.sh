af5e1121160a56a9f2b2762f29e061f1d68154be
Pascal Gollor <kalle@pgollor.de>
2017-12-20 00:05:18 +0100

    add admin user

--- generate_password.sh
+++ generate_password.sh
@@ -2,6 +2,8 @@
 
 pw1=$(</dev/urandom tr -dc A-Za-z0-9 | head -c 28)
 pw2=$(</dev/urandom tr -dc A-Za-z0-9 | head -c 28)
+pw3=$(</dev/urandom tr -dc A-Za-z0-9 | head -c 28)
 
 sed -i "/^NEXTCLOUD_DATABASE_ROOT/c\\\NEXTCLOUD_DATABASE_ROOT=${pw1}" nextcloud.conf
 sed -i "/^NEXTCLOUD_DATABASE_PASSWORD/c\\\NEXTCLOUD_DATABASE_PASSWORD=${pw2}" nextcloud.conf
+sed -i "/^NEXTCLOUD_ADMIN_PASSWORD/c\\\NEXTCLOUD_ADMIN_PASSWORD=${pw3}" nextcloud.conf
