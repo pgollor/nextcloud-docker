f640c79e4f025aa8c85245849ea79966f936baf4
Pascal Gollor <kalle@pgollor.de>
2017-12-19 23:54:48 +0100

    generate passwort gen script

---
+++ generate_password.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+pw1=$(</dev/urandom tr -dc A-Za-z0-9 | head -c 28)
+pw2=$(</dev/urandom tr -dc A-Za-z0-9 | head -c 28)
+
+sed -i "/^NEXTCLOUD_DATABASE_ROOT/c\\\NEXTCLOUD_DATABASE_ROOT=${pw1}" nextcloud.conf
+sed -i "/^NEXTCLOUD_DATABASE_PASSWORD/c\\\NEXTCLOUD_DATABASE_PASSWORD=${pw2}" nextcloud.conf
