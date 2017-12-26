8ad7684b67f8305752b627ba728d51e3f80010da
Pascal Gollor <kalle@pgollor.de>
2017-12-26 19:36:41 +0100

    fix db backup freezing

--- backup.sh
+++ backup.sh
@@ -12,22 +12,21 @@ mkdir -p ${backupDir}/db
 # current date
 currentDate=$(date +"%Y-%m-%d_%H-%M-%S")
 
-
 # backup mysql
 dbFile="${backupDir}/db/${currentDate}.sql"
-docker-compose exec nextcloud-database sh -c 'exec mysqldump --lock-tables --default-character-set=utf8mb4 -uroot -p"${MYSQL_ROOT_PASSWORD}" ${MYSQL_DATABASE}' > ${dbFile}
+docker-compose exec nextcloud-database sh -c 'mysqldump --lock-tables --default-character-set=utf8mb4 -uroot -p"${MYSQL_ROOT_PASSWORD}" ${MYSQL_DATABASE}' > ${dbFile}
 sed -i "/^mysqldump: \\[Warning\\]/d" ${dbFile}
 tar -cj ${dbFile} -f "${dbFile}.tbz2"
 rm ${dbFile}
 
 # backup app data
-tar -C data --exclude=app/data -cj app -f "${backupDir}/app/${currentDate}.tbz2"
+tar -C data --exclude=app/data -pcj app -f "${backupDir}/app/${currentDate}.tbz2"
 
-# backup data
-sudo tar -C data -cj app/data -f "${backupDir}/data/${currentDate}.tbz2"
+# backup data as root
+sudo tar -C data -pcj app/data -f "${backupDir}/data/${currentDate}.tbz2"
 
 # backup config files
-tar -cj data/conf nextcloud.conf -f "${backupDir}/conf/${currentDate}.tbz2"
+tar -pcj data/conf nextcloud.conf -f "${backupDir}/conf/${currentDate}.tbz2"
 
 # delete all files older 30 days
 #find ${backupDir} -iname "*.tbz2" -type f -mtime +10 -exec rm {} \; > /dev/null
